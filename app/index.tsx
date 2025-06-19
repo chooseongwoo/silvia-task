b908fbd2dc5812a6f9a3da8730b366bd858c7ab5
추성우 <cntjddn715@gmail.com>
2025-06-19 17:46:02 +0900

    chore :: 기초 세팅

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { Stack } from "expo-router";
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function HomeScreen() {
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <Stack.Screen />
+      <Text>안녕하세요</Text>
+    </SafeAreaView>
+  );
+}
